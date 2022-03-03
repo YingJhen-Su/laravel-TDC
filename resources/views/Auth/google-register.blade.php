329fa4ce6ca86ef910a394cb7762f152d659597a
YingJhen-Su <clearsky0330@gmail.com>
2022-03-03 16:39:53 +0000

    [add] 增加註冊、登入blade套版

---
+++ resources/views/Auth/google-register.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sign Up V4</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="assets/favicon.ico"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <!--===============================================================================================-->
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+    <div class="container px-4 px-lg-5">
+        <a class="navbar-brand" href="index.html">Taiwanese Drama Club</a>
+    </div>
+</nav>
+<div class="limiter">
+    <div class="container-login100" style="background-image: url('assets/img/bg-01.jpg');">
+        <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+            <form class="login100-form validate-form">
+                        <span class="login100-form-title p-b-49">
+                            Create
+                        </span>
+
+                <div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+                    <span class="label-input100">Username</span>
+                    <input class="input100" type="text" name="username" placeholder="Type your username">
+                    <span class="focus-input100" data-symbol="&#xf007;"></span>
+                </div>
+
+                <div class="text-right p-t-8 p-b-31">
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <div class="wrap-login100-form-btn">
+                        <div class="login100-form-bgbtn"></div>
+                        <button class="login100-form-btn">
+                            Sign Up Using Google
+                        </button>
+                    </div>
+                </div>
+
+                <div class="flex-col-c p-t-30">
+                            <span class="txt1 p-b-17">
+                                Or Back To
+                            </span>
+
+                    <a href="#" class="txt2">
+                        Login
+                    </a>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<div id="dropDownSelect1"></div>
+
+<script src="js/main.js"></script>
+
+</body>
+</html>
