2a155f94baa1d4d286c8c16fbfcf590a34bdea1d
YingJhen-Su <clearsky0330@gmail.com>
2022-03-12 17:29:45 +0000

    [mod] Social Model 增加fillable設定

--- app/Models/Social.php
+++ app/Models/Social.php
@@ -9,6 +9,25 @@ class Social extends Model
 {
     use HasFactory;
 
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'user_id',
+    'platform',
+    'social_id',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+  ];
+
     public function user() {
       return $this->belongsTo(User::class);
     }
